1af984c14d89b0dd46be2c8a67af8f6ba4c9690b
¨Eduardo-Abad¨ <abad@onliwebs.com>
2016-06-10 15:42:36 -0700

    se agregaron los hiddens a la vista 1

--- vistas/encuestaCorta/1.php
+++ vistas/encuestaCorta/1.php
@@ -11,7 +11,7 @@
 	include '../actions/actionRequerido.php';
 ?>
 <body onload = "document.eCorta1.aPaterno.focus() ">
-<form name="eCorta1" action="">
+<form name="eCorta1" action="" id="eCorta1">
    <div class="divPreguntas">
       <div class="divInputs">
           <label for="aPaterno"><strong>Apellido Paterno</strong></label>
@@ -19,10 +19,32 @@
           <label for="aPaterno"><strong>Apellido Materno</strong></label>
           <input type="text" id="aMaterno" class="txt-center" name="aMaterno" required value="<?=$ape_materno?>">
       </div>
-            <a href="2.php" class="txt-right"><span class="icon-arrow-right2"></span></a>
+        <div>
+            <input type="hidden" name="nombreForm" id="nombreForm" value="<?=$nombre?>">
+            <input type="hidden" name="edadForm" id="edadForm" value="<?=$edad?>">
+            <input type="hidden" name="originarioForm" id="originarioForm" value="<?=$originario?>">
+            <input type="hidden" name="estado_civilForm" id="estado_civilForm" value="<?=$estado_civil?>">
+            <input type="hidden" name="hijosForm" id="hijosForm" value="<?=$hijos?>">
+            <input type="hidden" name="estudiosForm" id="estudiosForm" value="<?=$estudios?>">
+            <input type="hidden" name="cruzandoForm" id="cruzandoForm" value="<?=$cruzando?>">
+            <input type="hidden" name="c_fecha_salio_casaForm" id="c_fecha_salio_casaForm" value="<?=$c_fecha_salio_casa?>">
+            <input type="hidden" name="c_por_donde_cruzoForm" id="c_por_donde_cruzoForm" value="<?=$c_por_donde_cruzo?>">
+            <input type="hidden" name="c_cuanto_cobranForm" id="c_cuanto_cobranForm" value="<?=$c_cuanto_cobran?>">
+            <input type="hidden" name="c_intentosForm" id="c_intentosForm" value="<?=$c_intentos?>">
+            <input type="hidden" name="vivia_euaForm" id="vivia_euaForm" value="<?=$vivia_eua?>">
+            <input type="hidden" name="v_anos_viviendoForm" id="v_anos_viviendoForm" value="<?=$v_anos_viviendo?>">
+            <input type="hidden" name="v_donde_viviaForm" id="v_donde_viviaForm" value="<?=$v_donde_vivia?>">
+            <input type="hidden" name="v_donde_lo_detuvieronForm" id="v_donde_lo_detuvieronForm" value="<?=$v_donde_lo_detuvieron?>">
+            <input type="hidden" name="tiempo_detenidoForm" id="tiempo_detenidoForm" value="<?=$tiempo_detenido?>">
+            <input type="hidden" name="porque_mas_3_diasForm" id="porque_mas_3_diasForm" value="<?=$porque_mas_3_dias?>">
+            <input type="hidden" name="victima_abusoForm" id="victima_abusoForm" value="<?=$victima_abuso?>">
+            <input type="hidden" name="conoce_derechosForm" id="conoce_derechosForm" value="<?=$conoce_derechos?>">
+            <input type="hidden" name="derechos_violados_porqueForm" id="derechos_violados_porqueForm" value="<?=$derechos_violados_porque?>">
+            <input type="hidden" name="servicio_moduloForm" id="servicio_moduloForm" value="<?=$servicio_modulo?>">
+        </div>
     </div>
+    <a href="2.php" class="txt-right"><span class="icon-arrow-right2"></span></a>
 </form>
-
 <?php
 include '../../vistas/footer.php';
 ?>
