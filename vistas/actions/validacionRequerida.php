811fc15c91c56e2412da213ef17e4733c9c183ea
¨Eduardo-Abad¨ <abad@onliwebs.com>
2016-06-10 14:21:35 -0700

    agregando una clase para las validaciones aparte de cada vista

---
+++ vistas/actions/validacionRequerida.php
@@ -0,0 +1,96 @@
+<?php
+    #####################################################
+	#AUTOR: Eduardo Abad Tinoco  (EAT)                 	#
+	#FECHA: 10 de Junio del 2016						#
+	#DESCRIPCIÓN: Trae el ultimo id          			#
+	#valida si no se ah completado alguno mas           #
+	#####################################################
+	#HISTORIAL DE MODIFICACIONES						#
+	#####################################################
+	// Incluimos el archivo de configuración
+	include '../../conexionBD.php';
+	// Requerimos la clase de usuarios
+	require '../../lib/preguntas.php';
+	$preguntas = new preguntas($datosConexionBD);
+    $i=0;
+    $id=[];
+    $ape_paterno = null;
+    $ape_materno = null;
+    $servicio_modulo = null;
+    $c_ape_paterno = null;
+    $c_ape_materno = null;
+    $c_servicio_modulo = null;
+
+    $ape_paterno = null;
+    $ape_materno = null;
+    $nombre = null;
+    $edad = null;
+    $originario = null;
+    $estado_civil = null;
+    $hijos = null;
+    $estudios = null;
+    $cruzando = null;
+    $c_fecha_salio_casa = null;
+    $c_por_donde_cruzo = null;
+    $c_cuanto_cobran = null;
+    $c_intentos = null;
+    $vivia_eua = null;
+    $v_anos_viviendo = null;
+    $v_donde_vivia = null;
+    $v_donde_lo_detuvieron = null;
+    $tiempo_detenido = null;
+    $porque_mas_3_dias = null;
+    $victima_abuso = null;
+    $conoce_derechos = null;
+    $derechos_violados_porque = null;
+    $servicio_modulo = null;
+    
+    $idR = $preguntas->consultaId();
+    while($row = $idR->fetch_assoc()) {
+			//Declaramos las variables de los atributos de mi clase usuario
+			//$id = $row['id_migrante'];
+         $i++;
+        $id[$i] = $row['id_migrante'];
+		}
+  //  echo "El id mas alto es: " . $id[$i];
+	//$preguntas = $preguntas->consultaPreguntas();
+	
+    //QUERY PARA PREGUNTAS LLENAS
+       $preguntas->id_migrante = $id[$i];
+		$preguntasR = $preguntas->consultarPreguntasxId();
+		//print_r($result);
+		while($row = $preguntasR->fetch_assoc()) {
+			//Declaramos las variables de los atributos de mi clase usuario
+			$c_ape_paterno = $row['ape_paterno'];
+			$c_ape_materno = $row['ape_materno'];
+            $servicio_modulo = $row['servicio_modulo'];
+		}
+        if($c_servicio_modulo==""){
+           // echo "servicio es nada";
+        }else{
+            //echo "Servicio esta lleno";
+            $nuevoId = $preguntas->nuevoId();
+             $idR = $preguntas->consultaId();
+            while($row = $idR->fetch_assoc()) {
+                    //Declaramos las variables de los atributos de mi clase usuario
+                    //$id = $row['id_migrante'];
+                 $i++;
+                $id[$i] = $row['id_migrante'];
+                }
+           // echo "El id mas ahora es alto es: " . $id[$i];
+        }
+    //echo "Servicios del modulo: " . $servicio_modulo;
+    //FIN DE QUERY PARA PREGUNTAS LLENAS
+
+/*Nueva consulta de id incrmeentado*/
+ $preguntas->id_migrante = $id[$i];
+		$preguntasR = $preguntas->consultarPreguntasxId();
+		//print_r($result);
+		while($row = $preguntasR->fetch_assoc()) {
+			//Declaramos las variables de los atributos de mi clase usuario
+			$ape_paterno = $row['ape_paterno'];
+			$ape_materno = $row['ape_materno'];
+            $servicio_modulo = $row['servicio_modulo'];
+		}
+/*Fin de Nueva consulta de id incrmeentado*/
+?>
--- vistas/encuestaCorta/1.php
+++ vistas/encuestaCorta/1.php
@@ -1,6 +1,6 @@
 <?php
 	#####################################################
-	#AUTOR: Eduardo Abad Tinoco                 (MHMB)	#
+	#AUTOR: Eduardo Abad Tinoco                 (EAT)	#
 	#FECHA: 10 de Junio del 2016						#
 	#DESCRIPCIÓN: INGRESO DE DATOS          			#
 	#                           						#
@@ -8,71 +8,10 @@
 	#HISTORIAL DE MODIFICACIONES						#
 	#####################################################
 	include '../../vistas/header.php';
-	
-	// Incluimos el archivo de configuración
-	include '../../conexionBD.php';
-	// Requerimos la clase de usuarios
-	require '../../lib/preguntas.php';
-	
-	//Instanciamos nuestra clase usuarios
-	$preguntas = new preguntas($datosConexionBD);
-    $i=0;
-    $id=[];
-    $ape_paterno = null;
-    $ape_materno = null;
-    $servicio_modulo = null;
-    $c_ape_paterno = null;
-    $c_ape_materno = null;
-    $c_servicio_modulo = null;
-    
-    $idR = $preguntas->consultaId();
-    while($row = $idR->fetch_assoc()) {
-			//Declaramos las variables de los atributos de mi clase usuario
-			//$id = $row['id_migrante'];
-         $i++;
-        $id[$i] = $row['id_migrante'];
-		}
-  //  echo "El id mas alto es: " . $id[$i];
-	//$preguntas = $preguntas->consultaPreguntas();
-	
-    //QUERY PARA PREGUNTAS LLENAS
-       $preguntas->id_migrante = $id[$i];
-		$preguntasR = $preguntas->consultarPreguntasxId();
-		//print_r($result);
-		while($row = $preguntasR->fetch_assoc()) {
-			//Declaramos las variables de los atributos de mi clase usuario
-			$c_ape_paterno = $row['ape_paterno'];
-			$c_ape_materno = $row['ape_materno'];
-            $c_servicio_modulo = $row['servicio_modulo'];
-		}
-        if($c_servicio_modulo==""){
-           // echo "servicio es nada";
-        }else{
-            //echo "Servicio esta lleno";
-            $nuevoId = $preguntas->nuevoId();
-             $idR = $preguntas->consultaId();
-            while($row = $idR->fetch_assoc()) {
-                    //Declaramos las variables de los atributos de mi clase usuario
-                    //$id = $row['id_migrante'];
-                 $i++;
-                $id[$i] = $row['id_migrante'];
-                }
-           // echo "El id mas ahora es alto es: " . $id[$i];
-        }
-    //echo "Servicios del modulo: " . $servicio_modulo;
-    //FIN DE QUERY PARA PREGUNTAS LLENAS
 
-/*Nueva consulta de id incrmeentado*/
- $preguntas->id_migrante = $id[$i];
-		$preguntasR = $preguntas->consultarPreguntasxId();
-		//print_r($result);
-		while($row = $preguntasR->fetch_assoc()) {
-			//Declaramos las variables de los atributos de mi clase usuario
-			$ape_paterno = $row['ape_paterno'];
-			$ape_materno = $row['ape_materno'];
-            $servicio_modulo = $row['servicio_modulo'];
-		}
-/*Fin de Nueva consulta de id incrmeentado*/
+	include '../actions/validacionRequerida.php';
+	//Instanciamos nuestra clase usuarios
+
 ?>
 <body onload = "document.eCorta1.aPaterno.focus() ">
 <form name="eCorta1" action="">
